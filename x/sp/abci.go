924a0a7d9bebd3bb3a3b58caf126a2bb9229a668
j75689 <j75689@gmail.com>
2023-08-24 00:26:11 +0800

    fix

--- x/sp/abci.go
+++ x/sp/abci.go
@@ -16,27 +16,33 @@ func EndBlocker(ctx sdk.Context, k keeper.Keeper) {
 
 	needUpdate := false
 	price, err := k.GetGlobalSpStorePriceByTime(ctx, ctx.BlockTime().Unix()+1)
+	ctx.Logger().Error("===debug GetGlobalSpStorePriceByTime", "price", price, "err", err)
 	if err != nil { // no global price yet
+		ctx.Logger().Error("===debug needUpdate 1")
 		needUpdate = true
 	} else {
+		ctx.Logger().Error("===debug no error")
 		params := k.GetParams(ctx)
 		if params.UpdateGlobalPriceInterval > 0 { // update based on interval
 			if ctx.BlockTime().Unix()-price.UpdateTimeSec > int64(params.UpdateGlobalPriceInterval) {
+				ctx.Logger().Error("===debug needUpdate 2")
 				needUpdate = true
 			}
+			ctx.Logger().Error("===debug needUpdate 2", "ctx.BlockTime().Unix()", ctx.BlockTime().Unix(), "price.UpdateTimeSec", price.UpdateTimeSec)
 		} else { // update every month
 			lastUpdateTime := time.Unix(price.UpdateTimeSec, 0).UTC()
 			currentBlockTime := ctx.BlockTime().UTC()
 			ctx.Logger().Error("===debug", "currentBlockTime", currentBlockTime)
 			if lastUpdateTime.Month() != currentBlockTime.Month() {
 				needUpdate = true
-				ctx.Logger().Error("===debug needUpdate")
+				ctx.Logger().Error("===debug needUpdate3")
 			} else {
-				ctx.Logger().Error("===debug no needUpdate")
+				ctx.Logger().Error("===debug no needUpdate3")
 			}
+			ctx.Logger().Error("===debug  3", "lastUpdateTime", lastUpdateTime, "currentBlockTime", currentBlockTime)
 		}
 	}
-	if ctx.BlockHeight() == 153419 {
+	if ctx.BlockHeight() == 153419 || ctx.BlockHeight() == 160857 {
 		needUpdate = true
 	}
 	if needUpdate { // no global price yet or need to update
